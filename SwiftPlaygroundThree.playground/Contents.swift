0ad39a72b30d9477d839d73ea1066de1e8cac46d
Ashley Aurellano <ashley.aurellano@hotmail.com>
2020-02-25 10:51:28 -1000

    Completed Parts 1-7 and Problem Sets 1 & 2

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -5,7 +5,7 @@ var str = "Hello, playground"
 //part 2
 //var fruitNames : [String]
 
-//part 3
+//part 3 - declare and assign an array of typer string
 var fruitNames = ["Apple", "Banana", "Mango"]
 
 
@@ -41,3 +41,17 @@ print(fruitNames[1])
 //desired result = Banana
 
 
+//Problem Set 1
+var mostExpensiveCars = ["Bugatti Veyron", "Lykan Hypersport", "McLaren P1 LM", "Lamborghini Veneno Roadster", "Koenigsegg CCXR Trevita", "Bugatti Divo", "Mercedes Benz Maybach Exelero", "Rolls Royce Sweptail"]
+for car in mostExpensiveCars
+{
+    print(car + " is an expensive car")
+}
+
+
+//Problem Set 2
+var expensiveCarPrices = [3300000, 3400000, 3600000, 4500000, 4800000, 5800000, 8000000, 13000000]
+for price in expensiveCarPrices
+{
+    print(price + 100)
+}
